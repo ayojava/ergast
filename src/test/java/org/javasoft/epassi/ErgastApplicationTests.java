610919844d364fd072143a69281582b48f198113
ayojava <ayojava@icloud.com>
2023-08-10 03:56:37 +0300

    Work In progress

--- src/test/java/org/javasoft/epassi/ErgastApplicationTests.java
+++ src/test/java/org/javasoft/epassi/ErgastApplicationTests.java
@@ -1,13 +1,114 @@
 package org.javasoft.epassi;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.javasoft.epassi.payload.ergast.seasons.Season;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.javasoft.epassi.Data.ALL_SEASONS_DATA;
+import static org.javasoft.epassi.api.ClientAPI.ALL_SEASONS_API;
+import static org.javasoft.epassi.api.ClientAPI.API;
+import static org.javasoft.epassi.api.ErgastAPI.SEASONS_API;
+import static org.javasoft.epassi.security.AuthenticationFilter.AUTH_TOKEN_HEADER;
+import static org.javasoft.epassi.security.AuthenticationFilter.AUTH_TOKEN_VALUE;
+import static org.springframework.http.HttpStatus.OK;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ErgastApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static WireMockServer wireMockServer;
+
+	@LocalServerPort
+	private int port;
+
+	private static HttpEntity requestEntity;
+
+
+	@Autowired
+	public TestRestTemplate testRestTemplate;
+
+
+	@BeforeAll
+	static void init() {
+		wireMockServer = new WireMockServer(
+				new WireMockConfiguration().port(7070)
+		);
+		wireMockServer.start();
+		WireMock.configureFor("localhost", 7070);
+
+		val requestHeaders = new HttpHeaders();
+		requestHeaders.add(AUTH_TOKEN_HEADER ,AUTH_TOKEN_VALUE);
+
+		requestEntity = new HttpEntity<>(null, requestHeaders);
 	}
+	@Test
+	void testAllSeasons() {
+
+		stubFor(any(urlPathEqualTo("/api/f1" + SEASONS_API))
+				.withQueryParam("limit", equalTo("1000"))
+				.withQueryParam("offset", equalTo("0"))
+				.willReturn(aResponse()
+						.withBody(ALL_SEASONS_DATA)
+						.withStatus(OK.value())));
+
+		String path = createUrlWithPort(ALL_SEASONS_API);
+
+		ResponseEntity<List<Season>> responseEntity = testRestTemplate.exchange(path, HttpMethod.GET, requestEntity, new ParameterizedTypeReference<>() {
+		});
+		List<Season> entityBody = responseEntity.getBody();
+
+		assertThat(200 ,is(responseEntity.getStatusCodeValue()));
+
+		assertThat(entityBody, is(notNullValue()));
+
+		entityBody.forEach(System.out::println);
+		Assertions.assertThat(entityBody)
+				.hasSize(74)
+				.contains(new Season("2023" ,"https://en.wikipedia.org/wiki/2023_Formula_One_World_Championship"))
+				.contains(new Season("1950" ,"http://en.wikipedia.org/wiki/1950_Formula_One_season"));
+
+
+
+		verify(getRequestedFor(urlPathEqualTo("/api/f1"+ SEASONS_API))
+				.withQueryParam("limit", equalTo("1000"))
+				.withQueryParam("offset", equalTo("0")));
+	}
+
+
+
+
+
+
+	    private String createUrlWithPort(String uri) {
+        final val stringBuilder = new StringBuilder();
+        return stringBuilder.append("http://localhost")
+                .append(":")
+                .append(port)
+                .append("/ergast")
+                .append(API)
+                .append(uri)
+                .toString();
+    }
 
 }
